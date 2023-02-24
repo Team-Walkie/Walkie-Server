ec0d119c1c9943e171a24f7189682094882dcd58
신승민 <tmdals8798@gmail.com>
2023-02-24 13:55:22 +0000

    init: 스프링 프로젝트 생성
    Co-authored-by: bngsh <hee132@korea.ac.kr>

---
+++ src/main/java/com/whyranoid/walkie/WalkieApplication.java
@@ -0,0 +1,13 @@
+package com.whyranoid.walkie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whyranoid/walkie/WalkieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whyranoid.walkie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
