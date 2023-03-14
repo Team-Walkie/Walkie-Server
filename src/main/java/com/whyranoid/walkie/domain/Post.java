30c477dbce21f4bdefdef5c40c9d5c159d4c96e1
Seungmin-develop <tmdals8798@gmail.com>
2023-03-14 16:24:01 +0900

    feat: 유저, 게시글 도메인 작성
    Co-authored-by: bngsh <hee132@korea.ac.kr>

---
+++ src/main/java/com/whyranoid/walkie/domain/Post.java
@@ -0,0 +1,46 @@
+package com.whyranoid.walkie.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    @Id
+    @Column(name = "post_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @NotNull
+    @Column(name = "photo", nullable = false)
+    private String photo;
+
+    @Column(name = "content")
+    private String content;
+
+    @NotNull
+    @Column(name = "date", nullable = false)
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private Walkie user;
+
+    @ManyToMany
+    @JoinTable(name = "walkie_post_like",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<Walkie> liker = new ArrayList<>();
+
+    // private Long historyId;
+}
---
+++ src/main/java/com/whyranoid/walkie/domain/Walkie.java
@@ -0,0 +1,57 @@
+package com.whyranoid.walkie.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter // 클래스 레벨에 선언할 경우 모든 필드에 접근자 자동 생성
+@NoArgsConstructor // 파라미터가 없는 기본 생성자 생성
+@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자 생성
+public class Walkie {
+    @Id // Entity의 primary key임을 명시
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성을 데이터베이스에 위임, mysql의 경우 auto-increment가 기본
+    private Long userId;
+
+    @NotNull
+    @Column(name = "user_name", nullable = false)
+    @Size(min = 1, max = 15) // 임의로 지정해 놓은 것
+    private String userName;
+
+    @Column(name = "profile_img")
+    private String profileImg;
+
+    @NotNull
+    @Column(name = "status", nullable = false)
+    private Character status;
+
+    @NotNull
+    @Column(name = "auth_id", nullable = false)
+    private String authId;
+
+    @OneToMany
+    @JoinTable(name = "walkie_post",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<Post> posts = new ArrayList<>();
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setProfileImg(String profileImg) {
+        this.profileImg = profileImg;
+    }
+
+    public void setStatus(Character status) {
+        this.status = status;
+    }
+}
