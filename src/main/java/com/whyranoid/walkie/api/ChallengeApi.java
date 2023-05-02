b70b7e4032f0bf71534a4dd8d5403736c623c8ac
Seungmin-develop <tmdals8798@gmail.com>
2023-05-02 20:09:16 +0900

    feat: 로컬에서 유저의 challenge 가져오기 테스트 완료

--- src/main/java/com/whyranoid/walkie/api/ChallengeApi.java
+++ src/main/java/com/whyranoid/walkie/api/ChallengeApi.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +20,9 @@ public class ChallengeApi {
     HttpHeaders httpHeaders = new HttpHeaders();
     private final ChallengeService challengeService;
 
-    @PostMapping("/newChallenges")
+    @GetMapping("/newChallenges")
     public ResponseEntity getChallenges(@RequestParam("userId") Long userId) {
+        // 본인 정보를 가져오는 로직이 필요
         List<Challenge> newChallenges = challengeService.getChallenges(userId, 'N');
         return new ResponseEntity<>(newChallenges, httpHeaders, HttpStatus.OK);
     }
--- src/main/java/com/whyranoid/walkie/repository/ChallengeRepository.java
+++ src/main/java/com/whyranoid/walkie/repository/ChallengeRepository.java
@@ -14,7 +14,8 @@ public class ChallengeRepository {
     private final EntityManager em;
 
     public List<Challenge> getChallenges(Long userId) {
-        List<Challenge> challenges = em.createQuery("select c from Challenge c join ChallengeStatus s on c.challengeId=s.challenge.challengeId where s.walkie.userId = :userId")
+        List<Challenge> challenges = em.createQuery("select c from Challenge c left join ChallengeStatus s " +
+                        "on c.challengeId=s.challenge.challengeId where s.walkie.userId = :userId")
                 .setParameter("userId", userId)
                 .getResultList();
 
