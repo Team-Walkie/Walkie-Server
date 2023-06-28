252832bcb3af177d43b4b4bc6a1055f21d98c056
bngsh <hee132@gmail.com>
2023-06-28 23:04:34 +0900

    feat: #10 WalkingController 추가

---
+++ src/main/java/com/whyranoid/walkie/controller/WalkingController.java
@@ -0,0 +1,25 @@
+package com.whyranoid.walkie.controller;
+
+import com.whyranoid.walkie.dto.request.WalkingStartRequest;
+import com.whyranoid.walkie.dto.response.WalkingStartResponse;
+import com.whyranoid.walkie.service.WalkingService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "WalkingController")
+@RequiredArgsConstructor
+@RequestMapping("/api/walk")
+@RestController
+public class WalkingController {
+
+    private final WalkingService walkingService;
+
+    @PutMapping("/start")
+    public WalkingStartResponse startWalking(@RequestBody WalkingStartRequest walkingStartRequest) {
+        return walkingService.startWalking(walkingStartRequest);
+    }
+}
