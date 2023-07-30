7e8d4ea2274ef22cc7e71d7b47da704a949042ff
bngsh <hee132@gmail.com>
2023-07-30 23:21:10 +0900

    feat: #12 FollowRepository 구현 (JPA, querydsl)
    - 팔로잉 리스트 가져오기 기능 추가
    - 팔로워 리스트 가져오기 기능 추가

---
+++ src/main/java/com/whyranoid/walkie/repository/querydsl/FollowRepositoryQuerydslImpl.java
@@ -0,0 +1,52 @@
+package com.whyranoid.walkie.repository.querydsl;
+
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.whyranoid.walkie.dto.QWalkieDto;
+import com.whyranoid.walkie.dto.WalkieDto;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.whyranoid.walkie.domain.QFollow.follow;
+import static com.whyranoid.walkie.domain.QWalkie.walkie;
+
+@RequiredArgsConstructor
+public class FollowRepositoryQuerydslImpl implements FollowRepositoryQuerydsl {
+
+    private final JPAQueryFactory queryFactory;
+
+
+    @Override
+    public List<WalkieDto> findFollowerList(Long whoseId) {
+        return findFollowerList(whoseId, false, findFollowerIdList(whoseId));
+    }
+
+    @Override
+    public List<WalkieDto> findFollowingList(Long whoseId) {
+        return  findFollowerList(whoseId, true, findFollowedIdList(whoseId));
+    }
+
+    public List<WalkieDto> findFollowerList(Long whoseId, boolean isFollowing, JPQLQuery<Long> followIdList) {
+        return queryFactory
+                .select(new QWalkieDto(walkie))
+                .from(walkie)
+                .where(walkie.userId.in(followIdList))
+                .fetch();
+    }
+
+    public JPQLQuery<Long> findFollowerIdList(Long whoseId) {
+        return JPAExpressions
+                .select(follow.follower.userId)
+                .from(follow)
+                .where(follow.followed.userId.eq(whoseId));
+    }
+
+    public JPQLQuery<Long> findFollowedIdList(Long whoseId) {
+        return JPAExpressions
+                .select(follow.followed.userId)
+                .from(follow)
+                .where(follow.follower.userId.eq(whoseId));
+    }
+}
