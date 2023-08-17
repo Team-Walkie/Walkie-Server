74fe8cbf9dd24980c7099b5c15e47b3f3588d235
bngsh <hee132@gmail.com>
2023-08-17 22:48:34 +0900

    feat: #19 WalkingLike 실시간 좋아요 엔티티/레포지토리 추가

---
+++ src/main/java/com/whyranoid/walkie/repository/WalkingLikeRepository.java
@@ -0,0 +1,7 @@
+package com.whyranoid.walkie.repository;
+
+import com.whyranoid.walkie.domain.WalkingLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WalkingLikeRepository extends JpaRepository<WalkingLike, Long> {
+}
