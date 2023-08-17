71e5d93dfdbf52372746f45fc2bf7c17655a2a4b
bngsh <hee132@gmail.com>
2023-08-17 22:49:20 +0900

    feat: #19 WalkingLikeDto 클래스 추가

---
+++ src/main/java/com/whyranoid/walkie/dto/WalkingLikeDto.java
@@ -0,0 +1,24 @@
+package com.whyranoid.walkie.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WalkingLikeDto {
+
+    @Schema(description = "요청 필수 파라미터 - 좋아요 누른 유저의 워키 아이디", requiredMode = Schema.RequiredMode.REQUIRED, example = "3")
+    private Long senderId;
+
+    @Schema(description = "요청 필수 파라미터 - 좋아요 받은 유저의 워키 아이디", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
+    private Long receiverId;
+
+    @Builder
+    public WalkingLikeDto(Long senderId, Long receiverId) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+    }
+}
