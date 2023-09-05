efe1a70bf7d9e4dde1496d12ec6f439c5894244b
Seungmin-develop <tmdals8798@gmail.com>
2023-09-06 01:19:35 +0900

    swagger 수정

--- src/main/java/com/whyranoid/walkie/controller/CommunityController.java
+++ src/main/java/com/whyranoid/walkie/controller/CommunityController.java
@@ -3,6 +3,8 @@ package com.whyranoid.walkie.controller;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.whyranoid.walkie.service.CommunityService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -26,8 +28,15 @@ public class CommunityController {
     HttpHeaders httpHeaders = new HttpHeaders();
 
     private final CommunityService communityService;
-    @Operation(summary = "게시글 올리기", description = "커뮤니티에 게시글을 업로드합니다. colorMode는 글자색으로 다음과 같이 표현합니다. -> (0: 검정색, 1: 흰색) historyFlag는 history 정보 포함 여부로 다음과 같이 표현합니다. -> (0: history 정보 미포함, 1: history 정보 포함)")
+    @Operation(summary = "게시글 올리기", description = "커뮤니티에 게시글을 업로드합니다. colorMode는 글자색으로 다음과 같이 표현합니다. -> (0: 검정색, 1: 흰색) historyContent는 history 정보를 표현합니다. 없으면 null 넘겨주시면 돼요")
     @ApiResponse(responseCode = "200", description = "업로드 성공", content = @Content(schema = @Schema(implementation = com.whyranoid.walkie.dto.response.ApiResponse.class)))
+    @Parameters({
+            @Parameter(name = "image", description = "업로드할 이미지 multipart", example = "image.jpg"),
+            @Parameter(name = "walkieId", description = "유저 아이디", example = "123"),
+            @Parameter(name = "content", description = "유저가 쓴 글의 내용", example = "아 상쾌하게 달렸다!"),
+            @Parameter(name = "colorMode", description = "글자색 카테고리(0은 검정, 1은 흰색)", example = "1"),
+            @Parameter(name = "historyContent", description = "운동기록 정보", example = "7.51km 01:03:45 08'29\"")
+    })
     @PostMapping(value = "/upload-post")
     public ResponseEntity uploadPost(
         @RequestParam("image")MultipartFile image,
